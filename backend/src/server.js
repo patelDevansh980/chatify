3ebee8723be22c77d19fceec15a2aa73e37409a5
devansh934 <devanshpatel185@gmail.com>
2025-11-09 13:14:37 +0530

    engine field is added in package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,6 +16,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
 
+
 // make ready for deployment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
